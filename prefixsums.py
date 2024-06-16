37a5ca5ff498d0160feefe3302cec516316ae677
Eli Rodrigues <66928418+erod101-1@users.noreply.github.com>
2024-06-16 14:59:56 -0400

    New Leetcode

--- prefixsums.py
+++ prefixsums.py
@@ -27,4 +27,24 @@ class NumArray(object):
             return 0
         return self.prefix[right] - self.prefix[left - 1] if left > 0 else self.prefix[right]
 
+# 724. Find Pivot Index
+# pivot index -> index where the sum of all the numbers strictly to the left is equal to sum strictly to the right
+
+class Solution(object):
+    def sumRange(self, left, right,prefix):
+        if right > len(prefix):   
+            return 0
+        return prefix[right] - prefix[left - 1] if left > 0 else prefix[right]
+
+    def pivotIndex(self, nums):
+        prefix_sum = [ ]
+        total = 0
+        for num in nums:
+            total += num
+            prefix_sum.append(total)
+        pivot = 0
+        for pivot in range(0,len(nums)):
+            if Solution.sumRange(self,0,pivot,prefix_sum) == Solution.sumRange(self,pivot,len(nums)-1,prefix_sum):
+                return pivot
+        return -1
 
